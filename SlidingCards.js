3dd4d276be5f99ceebcefbbca98ab2e7d627380e
Nixgit2000 <fourwaycoordination@gmail.com>
2023-03-23 18:23:45 -0400

    additional button functionality added. >> button still needs work.

--- SlidingCards.js
+++ SlidingCards.js
@@ -8,22 +8,19 @@ var cardsMargin = 20;
 var cardsWidth = cards.offsetWidth + cardsMargin;
 var numCards = 17
 var totalCardWidth = numCards * cardsWidth
-
 var Value = 0;
 var windowWidth = window.innerWidth
 var windowHeight = window.innerHeight
-
 var cardsInWindow = windowWidth/cardsWidth
 var windowSize = cardsInWindow*cardsWidth
 var windowSize2 = Math.floor(windowSize)
+
 addEventListener('load', function() {
 
     if (Value >=0) {
         leftButton.style.visibility = "hidden"
         left1Button.style.visibility = "hidden"
-        console.log(totalCardWidth)
-        console.log(windowWidth)
-        console.log(Math.floor(cardsInWindow))
+
     }
 })
 addEventListener('click', function() {
@@ -35,17 +32,24 @@ addEventListener('click', function() {
     if (Value >= 0) {
         leftButton.style.visibility = "hidden"
     }
-    if (Value < -(windowSize2-cardsWidth)) {
+    if (Value < -222) {
         left1Button.style.visibility = "visible"
-        console.log("ACTION!")
+
     }
-    if (Value >= -(windowSize2-cardsWidth)) {
+    if (Value >= -222) {
         left1Button.style.visibility = "hidden"
-        console.log("CANCEL!")
+
         }
     
-    if (Value <= -(totalCardWidth + cardsMargin - windowWidth)) {
+    if (Value < -(totalCardWidth + cardsMargin - windowWidth)) {
         rightButton.style.visibility = "hidden"
+        right1Button.style.visibility = "hidden"
+    }
+
+
+    if (Value >= -(totalCardWidth + cardsMargin - windowWidth)) {
+        rightButton.style.visibility = "visible"
+        right1Button.style.visibility = "visible"
     }
 })
 leftButton.addEventListener('click', function() { 
@@ -54,7 +58,7 @@ leftButton.addEventListener('click', function() {
     } if (Value <= 0) {
     Value += 222;
     cardsContainer.style.left = Value + 'px'
-    console.log(Value)}
+}
 })
 
 rightButton.addEventListener('click', function() {
@@ -63,23 +67,23 @@ rightButton.addEventListener('click', function() {
     } if (Value <= 0) {
     Value -= 222;
     cardsContainer.style.left = Value + 'px'
-    console.log(Value)}
+}
 })
 
 left1Button.addEventListener('click', function() { 
     if (Value > 0) {
         Value == 0
     } if (Value <= 0) {
-    Value += ((Math.floor(cardsInWindow))*cardsWidth);
-    cardsContainer.style.left = Value + 'px'
-    console.log(Value)}
-})
+        Value = 0;
+        cardsContainer.style.left = Value + 'px'
+    }}
+)
 
 right1Button.addEventListener('click', function() {
-    if (Value > 0) {
-        Value == 0
-    } if (Value <= 0) {
+    if (((Math.floor(cardsInWindow))*cardsWidth) > windowWidth) {
     Value -= ((Math.floor(cardsInWindow))*cardsWidth);
     cardsContainer.style.left = Value + 'px'
-    console.log(Value)}
+    console.log(Value)
+    }
+
 })

8498b76d4b12c8f9a24e70aae3f12808bab75642
Nixgit2000 <fourwaycoordination@gmail.com>
2023-03-23 17:06:15 -0400

    figured out left fast-advance algo. time to work on the right.

--- SlidingCards.js
+++ SlidingCards.js
@@ -15,50 +15,71 @@ var windowHeight = window.innerHeight
 
 var cardsInWindow = windowWidth/cardsWidth
 var windowSize = cardsInWindow*cardsWidth
+var windowSize2 = Math.floor(windowSize)
 addEventListener('load', function() {
 
     if (Value >=0) {
         leftButton.style.visibility = "hidden"
+        left1Button.style.visibility = "hidden"
         console.log(totalCardWidth)
         console.log(windowWidth)
-        console.log(cardsInWindow)
+        console.log(Math.floor(cardsInWindow))
     }
 })
 addEventListener('click', function() {
+
     if (Value < 0) {
     leftButton.style.visibility = "visible"
+    rightButton.style.visibility = "visible"
     }
     if (Value >= 0) {
         leftButton.style.visibility = "hidden"
     }
-    if (Value < 0) {
-        rightButton.style.visibility = "visible"
+    if (Value < -(windowSize2-cardsWidth)) {
+        left1Button.style.visibility = "visible"
+        console.log("ACTION!")
     }
+    if (Value >= -(windowSize2-cardsWidth)) {
+        left1Button.style.visibility = "hidden"
+        console.log("CANCEL!")
+        }
+    
     if (Value <= -(totalCardWidth + cardsMargin - windowWidth)) {
         rightButton.style.visibility = "hidden"
     }
 })
 leftButton.addEventListener('click', function() { 
+    if (Value > 0) {
+        Value == 0
+    } if (Value <= 0) {
     Value += 222;
     cardsContainer.style.left = Value + 'px'
-    console.log(Value)
+    console.log(Value)}
 })
 
 rightButton.addEventListener('click', function() {
+    if (Value > 0) {
+        Value == 0
+    } if (Value <= 0) {
     Value -= 222;
     cardsContainer.style.left = Value + 'px'
-    console.log(Value)
+    console.log(Value)}
 })
 
 left1Button.addEventListener('click', function() { 
+    if (Value > 0) {
+        Value == 0
+    } if (Value <= 0) {
     Value += ((Math.floor(cardsInWindow))*cardsWidth);
     cardsContainer.style.left = Value + 'px'
-    console.log(Value)
+    console.log(Value)}
 })
 
 right1Button.addEventListener('click', function() {
-
+    if (Value > 0) {
+        Value == 0
+    } if (Value <= 0) {
     Value -= ((Math.floor(cardsInWindow))*cardsWidth);
     cardsContainer.style.left = Value + 'px'
-    console.log(Value)
+    console.log(Value)}
 })
